e4153be88d76c172f542318b38e31b053adbffcd
baki gul <bbkgull@gmail.com>
2022-02-02 20:13:37 +0300

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+<main class="login-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <h3 class="card-header text-center">Login</h3>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login.custom') }}">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <input type="text" placeholder="Email" id="email" class="form-control" name="email" required autofocus>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
+                                @if ($errors->has('emailPassword'))
+                                <span class="text-danger">{{ $errors->first('emailPassword') }}</span>
+                                @endif
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="d-grid mx-auto">
+                                <button type="submit" class="btn btn-dark btn-block">Signin</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
